bc48908bff729c2b3c43763318bb2ad08aef3bf4
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-04-24 01:48:09 +0500

    Hosts Prblem of backend

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -8,8 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-insecure-key-for-dev')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'my-portfolio-k9q5.onrender.com,localhost,127.0.0.1').split(',')
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
